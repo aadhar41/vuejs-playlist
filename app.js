4bb45e6e1ffa024d07ca88e74a152230672a4ba1
Aadhar-windows-10 <aadhar41@gmail.com>
2019-04-09 09:33:34 +0530

    After #3 Data & Methods

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ new Vue({
     el: "#vue-app",
     data : {
         name: 'Aadhar',
-        job: 'Ninja'
+        job: 'Ninja',
+        website: 'http://google.com',
+        websiteTag: '<a href="http://google.com">Google.com</a>'
     },
     methods: {
-        greet: function (time) {  
+        greet: function (time='morning') {  
             
             return 'Good ' + time + ' ' + this.name
         }
