8debde49d30211349c942d9ce83211009585b8a8
Aadhar-windows-10 <aadhar41@gmail.com>
2019-04-09 08:57:23 +0530

    VueJs Instances, data & Methods

---
+++ app.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: "#vue-app",
+    data : {
+        name: 'Aadhar',
+        job: 'Ninja'
+    },
+    methods: {
+        greet: function (time) {  
+            
+            return 'Good ' + time + ' ' + this.name
+        }
+    }
+});
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Vuejs Playlist</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <script type = "text/javascript" src="vue.min.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="styles.css">
+</head>
+<body>
+    <div id="vue-app">
+        <h1>{{ greet('night') }}</h1>
+        <p>{{ name }}</p>
+        <p>{{ job }}</p>
+    </div>
+
+
+    <script src="app.js"></script>
+</body>
+</html>
