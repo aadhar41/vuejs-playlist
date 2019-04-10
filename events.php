e7a17397e2e42d3c34b79efc09e7a091d1e5eaa5
Aadhar-windows-10 <aadhar41@gmail.com>
2019-04-10 09:05:17 +0530

    after #5 - Events

---
+++ events.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Vuejs Playlist - events</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <script type = "text/javascript" src="vue.min.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="styles.css">
+</head>
+<body>
+    <div id="vue-app">
+        <h1>Events</h1>
+        
+        <button @click="add(1)">Add a Year</button>
+        <button v-on:click="substract(1)">Substract a year</button>
+
+        <button @dblclick="add(10)">Add 10 Year</button>
+        <button v-on:dblclick="substract(10)">Substract 10 year</button>
+        <p>My age is {{ age }}</p>
+
+        <div id="canvas" v-on:mousemove="updateXY">
+            {{x}},{{y}}
+        </div>
+    </div>
+
+
+    <script src="events.js"></script>
+</body>
+</html>
